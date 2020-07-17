dafae004ea121cddd5571e2afa2e33c7bb9367b3
MahdiHat791 <67454600+MahdiHat791@users.noreply.github.com>
2020-07-17 13:13:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,50 +3,17 @@ objects = [];
 status = "";
 
 function preload(){
-  fingers = createVideo('v2.mp4');
+  //fingers = createVideo('v2.mp4');
 }
 
 
 function setup() {
   canvas = createCanvas(550, 380);
   canvas.center();
-  //fingers.hide();
- // objectDetector = ml5.objectDetector('cocossd', modelLoaded);
- fingers.loop();
- fingers.speed(0.5);
-
   document.getElementById("status").innerHTML = "Status : Detecting Objects";
-}
-
-function modelLoaded() {
-  console.log("Model Loaded!")
-  status = true;
-}
-
-function gotResult(error, results) {
-  if (error) {
-    console.log(error);
-  }
-  console.log(results);
-  objects = results;
 }
 
 
 function draw() {
-  image(fingers, 0, 0, 550, 380);
-      if(status != "")
-      {
-        objectDetector.detect(fingers, gotResult);
-        for (i = 0; i < objects.length; i++) {
-          document.getElementById("status").innerHTML = "Status : Object Detected";
-          document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
- 
-          fill("#FF0000");
-          percent = floor(objects[i].confidence * 100);
-          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
-          noFill();
-          stroke("#FF0000");
-          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-        }
-      }
+  //image(fingers, 0, 0, 550, 380);
 }
