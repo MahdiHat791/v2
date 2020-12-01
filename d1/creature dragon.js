caf304237385311e852e9ee69d883c8592f2baef
MahdiHat791 <67454600+MahdiHat791@users.noreply.github.com>
2020-12-01 10:56:49 +0530

    Update creature dragon.js

--- d1/creature dragon.js
+++ d1/creature dragon.js
@@ -3,12 +3,11 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 
 function preload() {
     
-    game.load.image('sky', 'assets/deepblue.png');
-    game.load.image('dragonTexture', 'assets/dragon.png');
-    game.load.json('dragonMesh', 'assets/dragon.json');
-    
+    game.load.image('sky', 'assets/s1.jpg');
+    game.load.image('dragonTexture', 'assets/d1.png');
+    game.load.json('dragonMesh', 'assets/dragon.json');   
+    game.load.image('tail', 'assets/t2.png');   
 
-    
 }
 
 var dragon = null;
@@ -16,10 +15,14 @@ var dragon = null;
 function create() {
 
     game.add.image(0, 0, 'sky');
+    a = game.add.image(65, 380, 'tail');
+    a.scale.setTo(0.6,0.7);
+    game.scale.pageAlignHorizontally = true;
 
     dragon = game.add.creature(500, 370, 'dragonTexture', 'dragonMesh');
 
-    dragon.scale.set(25.0);
+    dragon.scale.set(23.0);
+    dragon.rotation = +6;
     
     dragon.play(true); //  true = loop
 
