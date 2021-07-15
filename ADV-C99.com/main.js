b6ec906eb050abdd80da74ba45cf4bbeb371e7d0
MahdiHat791 <67454600+MahdiHat791@users.noreply.github.com>
2021-07-15 16:00:15 +0530

    Add files via upload

---
+++ ADV-C99.com/main.js
@@ -0,0 +1,43 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = ""; 
+    recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+     console.log(event); 
+
+    var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+        speak();
+}
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = document.getElementById("textbox").innerHTML;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+}
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
