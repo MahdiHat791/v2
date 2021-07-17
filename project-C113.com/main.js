99ea0c32e8a37cf46f7aa24de8d54d4ba0e28a7d
MahdiHat791 <67454600+MahdiHat791@users.noreply.github.com>
2021-07-17 16:08:17 +0530

    Add files via upload

---
+++ project-C113.com/main.js
@@ -0,0 +1,39 @@
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas(640, 480);
+  canvas.position(150, 150);
+  video = createCapture(VIDEO);
+  video.hide();
+
+}
+
+function draw() {
+  image(video, 230, 150, 220, 200);
+
+  fill(255, 0, 0);
+  stroke(255, 0, 0);
+  circle(50, 50, 80);
+
+  circle(50, 430, 80);
+
+  circle(590, 50, 80);
+
+  circle(590, 430, 80);
+
+  fill(0, 128, 0);
+  stroke(0, 128, 0);
+  rect(90, 40, 460, 20);
+
+  rect(90, 420, 460, 20);
+
+  rect(40, 90, 20, 300);
+
+  rect(580, 90, 20, 300);
+}
+
+function take_snapshot(){    
+  save('student_name.png');
+}
