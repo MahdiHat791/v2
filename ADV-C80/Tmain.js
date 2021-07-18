51e89d89e89bf4bdeb5c2aa4cc448c2a41957d90
MahdiHat791 <67454600+MahdiHat791@users.noreply.github.com>
2021-07-18 19:16:24 +0530

    Add files via upload

---
+++ ADV-C80/Tmain.js
@@ -0,0 +1,71 @@
+
+name_of_the_student_array = [];
+    
+function submit()
+{
+    var display_student_array = [];
+
+    for (var j = 1; j <= 4; j++) 
+    {
+        var name_of_the_student = document.getElementById("name_of_the_student_"+j).value;
+        console.log(name_of_the_student);
+        name_of_the_student_array.push(name_of_the_student);
+    }
+
+    console.log(name_of_the_student_array);
+
+    var lenght_of_name_of_students_array = name_of_the_student_array.length;
+    console.log(lenght_of_name_of_students_array);
+
+    for (var k = 0; k < lenght_of_name_of_students_array; k++) 
+    {
+        display_student_array.push("<h4>NAME - "+ name_of_the_student_array[k] + "</h4>");
+        console.log(display_student_array);
+    }
+
+    console.log(display_student_array);
+    document.getElementById("display_name_with_commas").innerHTML = display_student_array;
+
+    var remove_commas = display_student_array.join(" ");
+    console.log(remove_commas);
+    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
+
+
+    document.getElementById("submit_button").style.display = "none";
+    document.getElementById("sort_button").style.display = "inline-block";
+
+}
+
+function sorting()
+{
+    name_of_the_student_array.sort();
+    console.log(name_of_the_student_array);
+
+    var display_student_array_sorting = [];
+
+    var lenght_of_name_of_students_array = name_of_the_student_array.length;
+    console.log(lenght_of_name_of_students_array);
+
+    for (var k = 0; k < lenght_of_name_of_students_array; k++) 
+    {
+        display_student_array_sorting.push("<h4>NAME - " + name_of_the_student_array[k] + "</h4>");
+        console.log(display_student_array_sorting);
+    }
+
+    var remove_commas = display_student_array_sorting.join(" ");
+    console.log(remove_commas);
+
+    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
+}
+
+
+//Additional activity
+function new_update()
+{
+    document.getElementById("display_name_without_commas").innerHTML = "<h1>" + name_of_the_student_array +"</h1>";
+}
+
+
+
+
+
