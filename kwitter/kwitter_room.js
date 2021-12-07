968b5b52087e57accc1c07a3efdde87b98c76e5a
MahdiHat791 <67454600+MahdiHat791@users.noreply.github.com>
2021-12-07 20:14:39 +0530

    Update kwitter_room.js

--- kwitter/kwitter_room.js
+++ kwitter/kwitter_room.js
@@ -1,12 +1,13 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyDtnDgEywiL2rL-67QZ2VW2Qoj76-eYYjI",
-  authDomain: "deletetest-132c7.firebaseapp.com",
-  databaseURL: "https://deletetest-132c7-default-rtdb.firebaseio.com",
-  projectId: "deletetest-132c7",
-  storageBucket: "deletetest-132c7.appspot.com",
-  messagingSenderId: "251442731867",
-  appId: "1:251442731867:web:eb8dd0bdc2ec1bb2dd187e"
+  apiKey: "AIzaSyBQyjrjTsIQsGMGcgu-cr1HjszcHi5ZWMk",
+  authDomain: "testkwitter.firebaseapp.com",
+  databaseURL: "https://testkwitter.firebaseio.com",
+  projectId: "testkwitter",
+  storageBucket: "testkwitter.appspot.com",
+  messagingSenderId: "624653701634",
+  appId: "1:624653701634:web:2cb9a8bd873f17d92d8d1b"
 };
+
 
 
   firebase.initializeApp(firebaseConfig);
